1d7f54bdca6154dc6fa6e60ac9dee1e023de16c1
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-07-22 17:06:00 +0100

    Configure redux store

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 
+import { Provider } from 'react-redux'
+import store from './app/store'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
