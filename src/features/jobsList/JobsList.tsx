8705372cfb1730f35745ef1574a83a865005c33c
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-08-07 19:19:01 +0100

    Add useFetchJobs hook

--- src/features/jobsList/JobsList.tsx
+++ src/features/jobsList/JobsList.tsx
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+
+import { RootState } from '../../app/rootReducer'
+import useFetchJobs from '../../hooks/useFetchJobs'
+
 import Container from '../../utils/container/Container'
 
-const JobsList = () => {
+const Loading = () => {
   return (
     <div className="pt-24 px-2">
       <Container>
         <div className="p-2 bg-white rounded">
           <div className="bg-white shadow-xs px-2">
-            <h2 className="text-lg font-medium">Job Title</h2>
+            <h2 className="text-lg font-medium">Loading..</h2>
           </div>
         </div>
+      </Container>
+    </div>
+  )
+}
+
+const OnError = ({ error }: { error: string }) => {
+  return (
+    <div className="pt-24 px-2">
+      <Container>
+        <div className="p-2 bg-white rounded">
+          <div className="bg-white shadow-xs px-2">
+            <h2 className="text-lg font-medium text-red-700">
+              Try to refresh the page.
+            </h2>
+            <h3>Error: {error}</h3>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+const JobsList = () => {
+  const [page, setPage] = useState(0)
+  const description = useSelector(
+    (state: RootState) => state.search.description,
+  )
+  const location = useSelector((state: RootState) => state.search.location)
+
+  const { isLoading, hasError, jobs } = useFetchJobs({
+    description,
+    location,
+    page,
+  })
+  console.log({ isLoading, hasError, jobs })
+
+  if (isLoading) return <Loading />
+  if (hasError) return <OnError error={hasError} />
+  return (
+    <div className="pt-24 px-2">
+      <Container>
+        <div className="p-2 bg-white rounded">{jobs.length}</div>
       </Container>
     </div>
   )
---
+++ src/hooks/useFetchJobs.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const BASE_URL =
+  'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+interface Job {
+  company: string
+  company_logo: string
+  company_url: string
+  created_at: string
+  description: string
+  how_to_apply: string
+  id: string
+  location: string
+  title: string
+  type: string
+  url: string
+}
+
+interface UseFetchJobsArgs {
+  description: string
+  location: string
+  page: number
+}
+
+interface UseFetchJobsReturn {
+  jobs: Job[]
+  isLoading: boolean
+  hasError: string | null
+}
+
+type UseFetchJobs = (arg: UseFetchJobsArgs) => UseFetchJobsReturn
+
+const useFetchJobs: UseFetchJobs = ({ description, location, page }) => {
+  const [jobs, setJobs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(null)
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    axios
+      .get(BASE_URL, {
+        params: { description, location, page },
+      })
+      .then(res => {
+        setJobs(res.data)
+        setIsLoading(false)
+        setHasError(null)
+      })
+      .catch(err => {
+        setJobs([])
+        setIsLoading(false)
+        setHasError(err.message)
+      })
+
+    return () => {}
+  }, [description, location, page])
+
+  return { jobs, isLoading, hasError }
+}
+
+export default useFetchJobs
