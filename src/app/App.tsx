30c3ac652c31361dfe560834f090f46f55947835
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-08-08 13:51:13 +0100

    Remove extra store provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import Content from '../components/Content'
-import { Provider } from 'react-redux'
-import store from './store'
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="app pt-48">
-        <Header />
-        <Content />
-      </div>
-    </Provider>
+    <div className="app pt-48">
+      <Header />
+      <Content />
+    </div>
   )
 }
 
