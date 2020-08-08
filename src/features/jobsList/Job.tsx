5ba153703b3ff0640a3230bb760cd2c5d076f3ae
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-08-08 14:51:16 +0100

    Fix Job card to go to job details

--- src/features/jobsList/Job.tsx
+++ src/features/jobsList/Job.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Job as JobType } from '../../hooks/useFetchJobs'
 
 const Job = ({ job }: { job: JobType }) => {
   return (
-    <div className="p-2 mb-2 rounded shadow-xs">
-      <div className="flex">
-        <h4 className="flex-grow text-sm text-gray-600">{job.company}</h4>
-        {job.company_logo && (
-          <img
-            className="h-8 w-8 object-contain"
-            src={job.company_logo}
-            alt={job.company}
-          />
-        )}
+    <Link to={`/job/${job.id}`}>
+      <div className="p-2 mb-2 rounded shadow-xs">
+        <div className="flex">
+          <h4 className="flex-grow text-sm text-gray-600">{job.company}</h4>
+          {job.company_logo && (
+            <img
+              className="h-8 w-8 object-contain"
+              src={job.company_logo}
+              alt={job.company}
+            />
+          )}
+        </div>
+        <h3 className="mt-1 text-2xl">{job.title}</h3>
+        <div className="flex">
+          <h4 className="text-sm text-gray-600 flex-grow">{job.location}</h4>
+          <h4 className="text-sm text-gray-600">{job.created_at}</h4>
+        </div>
       </div>
-      <h3 className="mt-1 text-2xl">{job.title}</h3>
-      <div className="flex">
-        <h4 className="text-sm text-gray-600 flex-grow">{job.location}</h4>
-        <h4 className="text-sm text-gray-600">{job.created_at}</h4>
-      </div>
-    </div>
+    </Link>
   )
 }
 
